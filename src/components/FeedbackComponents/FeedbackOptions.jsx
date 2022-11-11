e4378beaf30999c2eafe74411cc6e30d74c16843
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-11 10:38:06 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackComponents/FeedbackOptions.jsx
+++ src/components/FeedbackComponents/FeedbackOptions.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import { Button } from 'components/Section/Section.styled';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) =>
-  Object.keys(options).map(item => (
+  Object.keys(options).map(option => (
     <Button
       type="button"
-      name={item}
-      onClick={() => onLeaveFeedback(item)}
-      key={item}
-      className={item}
+      name={option}
+      onClick={() => onLeaveFeedback(option)}
+      key={option}
+      className={option}
     >
-      {item}
+      {option}
     </Button>
   ));
 
